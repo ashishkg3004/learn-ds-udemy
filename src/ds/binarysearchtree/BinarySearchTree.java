8214fcec42b78aa43b2bda8c4a32a7ccbf3c4ef6
ashishkg3004 <ashishkg3004@gmail.com>
2017-02-24 22:51:28 +0530

    Added theory for binary search tree

---
+++ src/ds/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,22 @@
+package ds.binarysearchtree;
+
+public class BinarySearchTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/**
+ * BST DS offers the search benefits of a sorted array as well as inserting and
+ * deleting benefits of linked list. Excellent for inserting, deleting and
+ * searching. Anything to the left of root or a node will be less than the node
+ * and anything to the right of the node will be greater than the node. If we
+ * try to insert same value as that of a node then it goes to the left of the
+ * node by default behaviour. The Complexity of BST is O(log n). For this to
+ * achieve the BST must be 'Balanced'. Balanced : For all the nodes, the
+ * difference between the heights of the left and the right subtrees must not be
+ * greater than 1.
+ */
